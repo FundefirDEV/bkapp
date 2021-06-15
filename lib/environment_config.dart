fce9b551f540a1d311905f3aec34667b990861cc
AlexanderRaydan <alexraydancode@gmail.com>
2021-06-15 15:34:04 -0300

    change env

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -18,7 +18,7 @@ class EnvironmentConfig {
   ///
   /// Environment PROD; QA; STAGE or DEV
   ///
-  static const String ENV = String.fromEnvironment('PROD', defaultValue: "PROD");
+  static const String ENV = String.fromEnvironment('ENV', defaultValue: "DEV");
 
   ///
   /// DSN for sentry
