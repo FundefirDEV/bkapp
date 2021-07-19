544b0c65ddd5e0dbae95acea732395ca6912012f
AlexanderRaydan <alexraydancode@gmail.com>
2021-07-19 12:54:35 -0300

    read contact permis

--- lib/src/widgets/modals/inviteModal/contacts/contact_list.dart
+++ lib/src/widgets/modals/inviteModal/contacts/contact_list.dart
@@ -52,7 +52,9 @@ class _ContactListState extends State<ContactList> {
 
   @override
   void initState() {
-    _populateContacts();
+    //_populateContacts();
+
+    refreshContacts();
     super.initState();
   }
 
@@ -293,7 +295,8 @@ class _ContactListState extends State<ContactList> {
       selectContact.forEach((contact) {
         newPartners.add( PartnerModel(
           firstname: contact.contact.displayName,
-          phone: contact.contact.phones.elementAt(0).value
+          phone: contact.contact.phones.elementAt(0).value.replaceAll(
+            RegExp(r'[!@#<>?":_`~;[\]\\|=+-\s\b|\b\s]'), '')
         ));
       });
 
